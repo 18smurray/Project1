7bfbacfc3087276f27baf7de3a492bd1b7523175
Ethan Hollis <e.hollis32@gmail.com>
2021-03-15 11:09:21 -0600

    Testing push for models

---
+++ Models/Appointment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace Project1.Models
+{
+    public class Timeslot
+    {
+       
+        public DateTime Date { get; set; }
+        public bool Booked { get; set; }
+
+    }
+}
