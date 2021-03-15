b9f96e0c7ba87d204004a86107462b81766dca07
18smurray <66044394+18smurray@users.noreply.github.com>
2021-03-15 13:36:37 -0600

    Lots of controller comments

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,15 +13,96 @@ namespace Project1.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        //Variable for the DB context
+        //Private IAppointmentRepository = _repository
+            //Will be used to refer to different DBContexts...
+            //_reposiotory.Timeslots and _repository.Appointments?
+
+        //Add in parameter IAppointmentRepository repository
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            //_repository = repository
         }
 
+
+        //Action for the Home Page
         public IActionResult Index()
         {
             return View();
         }
+
+        //SignUpPage - have to pass in available timeslots
+        //Do we need a get and a set to pass the time to the form???
+        //Have ID as a hidden field...?
+
+        /*[HttpGet]
+         * public IActionResult SignUpPage()
+         * {
+         *      //Only return timeslots that are not booked yet
+         *      return View(_repository.Timeslots
+         *          .OrderBy(t => t.Id ??? could use id)
+         *          .Where(t => t.Booked == false)
+         *          );
+         * }
+        */
+        /*[HttpPost] - sending timeslot to the form 
+         * public IActionResult SignUpPage(Timeslot.Id timeslotid)
+         * {
+         *      //Go to the form view and pass the timeslot data 
+         *      Could use Viewbag
+         *      @ViewBag.TimeId = timeslotid
+         *      return View("AppointmentForm" timeslotid)
+         * }
+        */
+
+        //AppointmentForm page (Can't get to from nav bar - only through the signuppage post method...?
+
+
+        //Not sure if we need this?
+        /*[HttpGet]
+         * public IActionResult AppointmentForm(Timeslot.Id timeslotid)
+         * {
+         *          return View(timeslotid)
+         * }
+        */
+
+        //For when the appointment form is submitted
+        /*[HttpPost]
+         * public IActionResult AppointmentForm(Timeslot.id timeslotid Appointment newappt)
+         * {
+         *          //Ensure model state is valid
+         *          if (ModelState.isValid)
+         *          
+         *          //Have to set timeslot id for the corresponding appointment...
+         *          
+         *          _repository.Appointments.Add(newappt);
+         *          _repository.SaveChanges();
+         *  
+         *          Need a method for adding new appointments to DB
+         *         
+         *          //Somehow set booked status for specific DB element
+         *          _repository.Timeslot.Where(timeId == timeslotid).Booked == true
+         * 
+         *          return View(ViewAppointments)
+         * }
+        */
+
+        
+        //View Appointments page
+         /*[HttpGet]
+         * public IActionResult SignUpPage()
+         * {
+         * 
+         *      I could format and return a list of strings here???
+         *      Have to grab time and date using the set timeslotid for the appointment
+         *      return View(
+         *      
+         *      )
+         *      
+         * }
+        */
+
 
 
 
