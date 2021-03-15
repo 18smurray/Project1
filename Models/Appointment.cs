5dc0b42225efad346dc2f5d4a6f6f864c132714f
18smurray <66044394+18smurray@users.noreply.github.com>
2021-03-15 11:46:37 -0600

    removed comments

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -9,15 +9,7 @@ namespace Project1.Models
 {
     public class Timeslot
     {
-        //Peter's testing comment
-
-        //Shannon's test comment
-        //I added this in my own dev branch
-
-        //Nate's test comment
-       
         public DateTime Date { get; set; }
         public bool Booked { get; set; }
-        //testing merge
     }
 }
