330e8d6d260b25aafaaad24f29268b3185c69508
Ethan Hollis <e.hollis32@gmail.com>
2021-03-15 19:40:51 -0600

    nullable appt

--- Models/Timeslot.cs
+++ Models/Timeslot.cs
@@ -12,7 +12,7 @@ namespace Project1.Models
         public int TimeslotID { get; set; }
         public DateTime Date { get; set; } //I'm unsure here if Datetime is what I'll want. May be easier to go with string for this?
         public bool Booked { get; set; } = false; //Is this redundant with teh appointment property below?? Could we just 
-        public Appointment Appointment { get; set; }
+        public Appointment? Appointment { get; set; }
     }
 }
 
